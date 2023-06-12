cca85b577a7062159087f5f5d8e8b1dc463825ad
Barış TÜRE <cerensolpan@gmail.com>
2023-06-13 00:19:21 +0300

    feat: Create card component and update HomeView

---
+++ src/components/Card/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img class="card__img" :src="product.image" :alt="product.id" />
+    <div class="card__body">
+      <span>{{ product.name }}</span>
+    </div>
+    <div class="card__footer">
+      <span>{{ product.price }} {{ product.currency }}</span>
+      <ButtonComponent
+        class="btn btn-block btn-primary"
+        text="Add Basket"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import './style.scss'
+import ButtonComponent from '../UI/Button/index.vue'
+
+export default {
+  name: 'CardComponent',
+  props: {
+    product: {
+      id: Number,
+      name: String,
+      price: Number,
+      currency: String,
+      image: String
+    }
+  },
+  components: {
+    ButtonComponent
+  },
+  setup() {
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div v-if="loading"><Loading /></div>
+    <div class="products" v-else>
+      <div v-if="products.length === 0">Product is not found</div>
+      <div v-else v-for="product in products" :key="product.id" class="products__card">
+        <Card :product="product" />
+      </div>
+    </div>
   </main>
 </template>
+
+<script>
+import Card from '../components/Card/index.vue'
+import { computed, onMounted } from 'vue'
+import store from '../store.js'
+import Loading from '../components/UI/LoadingCircle/index.vue'
+
+export default {
+  name: 'products-page',
+  components: {
+    Card,
+    Loading
+  },
+  setup() {
+    onMounted(() => {
+      store.dispatch('getProducts')
+    })
+    return {
+      products: computed(() => store.state.products),
+      loading: computed(() => store.state.loading)
+    }
+  }
+}
+</script>
+
+<style></style>
