4dab00f6bce8d81ce42f96e0bfb468f62100aaef
Barış TÜRE <cerensolpan@gmail.com>
2023-06-13 12:05:36 +0300

    feat: Create addProduct action

--- src/store.js
+++ src/store.js
@@ -1,12 +1,26 @@
 import axios from 'axios'
 import { createStore } from 'vuex'
+import { toast } from 'vue3-toastify'
+import { quantityCalculator } from './utils/quantityCalculator'
 
 const store = createStore({
   state: {
     products: [],
+    cart: [],
+    countInBasket: 0,
     loading: false
   },
   mutations: {
+    addProduct(state, product) {
+      const cartProductIndex = state.cart.findIndex((p) => p.id === product.id)
+      if (cartProductIndex !== -1) {
+        state.cart[cartProductIndex] = product
+      } else {
+        state.cart.push(product)
+      }
+      state.countInBasket = quantityCalculator(state.cart)
+    },
+
     setProducts(state, data) {
       state.products = data
     },
@@ -16,6 +30,40 @@ const store = createStore({
     }
   },
   actions: {
+    async addProduct({ commit, state }, product) {
+      const cartProduct =
+        state.cart.filter((p) => p.id == product.id).length === 0
+          ? { ...product, ...{ quantity: 0 } }
+          : state.cart.filter((p) => p.id == product.id)[0]
+      cartProduct.quantity += 1
+      await axios
+        .post('https://nonchalant-fang.glitch.me/order', [
+          {
+            id: cartProduct.id,
+            amount: cartProduct.quantity
+          }
+        ])
+        .then(() => {
+          commit('addProduct', cartProduct)
+          const notify = () => {
+            toast.success('Added to basket', {
+              autoClose: 2000
+            })
+          }
+          notify()
+        })
+        .catch((error) => {
+          if (error.response.data.message === 'out of stock') {
+            const notify = () => {
+              toast.error('Out of stock :(', {
+                autoClose: 2000
+              })
+            }
+            notify()
+          }
+        })
+    },
+
     async getProducts({ commit }) {
       commit('loadingStatus', true)
 
@@ -31,6 +79,10 @@ const store = createStore({
   getters: {
     getProducts: (state) => {
       return state.products
+    },
+
+    getCart: (state) => {
+      return state.cart
     }
   },
   modules: {}
