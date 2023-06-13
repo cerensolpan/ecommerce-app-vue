d79b1715fdfc2d6fb0a760071a2bbffcbb6c3aac
Barış TÜRE <cerensolpan@gmail.com>
2023-06-13 12:17:01 +0300

    feat: Create basket component and add to the header

---
+++ src/components/Basket/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <RouterLink class="basket" to="/checkout">
+    <div class="basket__img">
+      <img src="../../assets/basketImg.png" style="width: 2.5rem" />
+      <span v-if="countInBasket !== 0" class="basket__count">{{ countInBasket }}</span>
+    </div>
+
+    <span>Basket</span>
+  </RouterLink>
+</template>
+
+<script>
+import './style.scss'
+import { computed } from 'vue'
+import store from '../../store'
+import { RouterLink } from 'vue-router'
+
+export default {
+  name: 'BasketComponent',
+
+  setup() {
+    return {
+      countInBasket: computed(() => store.state.countInBasket),
+      RouterLink
+    }
+  }
+}
+</script>
